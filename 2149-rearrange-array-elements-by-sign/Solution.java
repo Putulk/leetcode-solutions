dce0d4921dadb66dfa27c3e53dcdc61819b9419a
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-30 10:39:47 +0530

    Add solution: 2149. Rearrange Array Elements by Sign (Medium)

--- 2149-rearrange-array-elements-by-sign/Solution.java
+++ 2149-rearrange-array-elements-by-sign/Solution.java
@@ -12,7 +12,7 @@ class Solution {
             }
         }
 
-        for(int i=; i<nums.length-1; i++){
+        for(int i=1; i<nums.length-; i++){
             nums[2*i]=pos[i];
             nums[2*i+1]=neg[i];
         }
