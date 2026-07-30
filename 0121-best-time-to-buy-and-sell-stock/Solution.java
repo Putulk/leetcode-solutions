be1324f5e830794d74e3ea26807940ca54957451
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-30 10:07:32 +0530

    Add solution: 0121. Best Time to Buy and Sell Stock (Easy)

--- 0121-best-time-to-buy-and-sell-stock/Solution.java
+++ 0121-best-time-to-buy-and-sell-stock/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
         int max = 0; 
-        int min = Integer.MIN_VALUE;
+        int min = ;
         for(int i=0; i<n; i++){
             if(prices[i] < min){
                 min = peices[i];
