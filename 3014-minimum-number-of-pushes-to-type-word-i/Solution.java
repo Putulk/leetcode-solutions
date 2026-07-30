63912ee5b54c5f777ff3537ee733488b17badb19
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-30 10:17:15 +0530

    Add solution: 3014. Minimum Number of Pushes to Type Word I (Easy)

--- 3014-minimum-number-of-pushes-to-type-word-i/Solution.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumPushes(String word) {
         int cost = 0;
         for(int i=0; i<word.size(); i++){
-            if(word.contain
+            if(word.
         }
     }
 }
