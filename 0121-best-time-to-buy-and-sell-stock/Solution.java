9320d955cfa5b2569d89055c0f43ac801c0c35c4
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-30 10:11:06 +0530

    Add solution: 0121. Best Time to Buy and Sell Stock (Easy)

--- 0121-best-time-to-buy-and-sell-stock/Solution.java
+++ 0121-best-time-to-buy-and-sell-stock/Solution.java
@@ -8,7 +8,7 @@ class Solution {
                 min = prices[i];
             }
             else{
-                max = Math.max()
+                max = Math.max(m)
             }
         }
         return max;
