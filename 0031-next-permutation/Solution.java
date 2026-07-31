32c4d7892f044768bf2db2ff13799775c0be92ef
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-31 11:53:01 +0530

    Add solution: 0031. Next Permutation (Medium)

--- 0031-next-permutation/Solution.java
+++ 0031-next-permutation/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int piv = -1; 
         for(int i=nums.length-1; i>piv; i--){
-            if(nums[i]<nums[i+1]){
+            if(nums[i]<nums[i+]){
                 piv = i;
                 break;
             }
