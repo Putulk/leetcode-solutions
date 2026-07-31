f9a377d3e8588c973b624b9c2723563a6f5f3d6d
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-31 12:12:07 +0530

    Add solution: 0031. Next Permutation (Medium)

--- 0031-next-permutation/Solution.java
+++ 0031-next-permutation/Solution.java
@@ -8,7 +8,7 @@ class Solution {
             }
         }
         if(piv == -1){
-            for (int i = 0; i < nums.length / ; i++) {
+            for (int i = 0; i < nums.length-; i++) {
             int temp = arr[i];
             arr[i] = arr[n - 1 - i];
             arr[n - 1 - i] = temp;
