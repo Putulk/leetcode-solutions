f2207d95eb96ef91317057fcd7fc56b2ba350736
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-31 12:21:46 +0530

    Add solution: 0031. Next Permutation (Medium)

--- 0031-next-permutation/Solution.java
+++ 0031-next-permutation/Solution.java
@@ -1,24 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int piv = -1; 
-        for(int i=nums.length-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
+        int piv = -1;
+
+        // Step 1: Find pivot
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 piv = i;
                 break;
             }
         }
-        if(piv == -1){
-            reverse(nums, 0, nums.length-1);
-        }
-        int i = piv+1;
-        int j = nums.length-1;
 
-        while(i<=j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++; j--;
+        // Step 2: If no pivot exists, reverse the whole array
+        if (piv == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
         }
+
+        // Step 3: Find the next greater element
+        for (int i = nums.length - 1; i > piv; i--) {
+            if (nums[i] > nums[piv]) {
+                swap(nums, i, piv);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the suffix
+        reverse(nums, piv + 1, nums.length - 1);
     }
 
     private void reverse(int[] arr, int start, int end) {
@@ -28,5 +35,10 @@ class Solution {
             end--;
         }
     }
-    
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
