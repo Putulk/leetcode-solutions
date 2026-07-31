e79da5f13f8fc7b063e6708d378f0a92dd4f5f65
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-31 12:15:09 +0530

    Add solution: 0031. Next Permutation (Medium)

--- 0031-next-permutation/Solution.java
+++ 0031-next-permutation/Solution.java
@@ -21,7 +21,7 @@ class Solution {
         }
     }
 
-    private void reverse(int[] arr, int start, int end) {
+    private void reverse(int[] nums, int start, int end) {
         while (start < end) {
             swap(arr, start, end);
             start++;
