17e3e5001982da4163d3705a882f08d2ab5d5dcd
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-07-31 12:12:31 +0530

    Add solution: 0031. Next Permutation (Medium)

--- 0031-next-permutation/Solution.java
+++ 0031-next-permutation/Solution.java
@@ -12,7 +12,7 @@ class Solution {
             int temp = nums[i];
             nums[i] = nums[nums.length - 1 - i];
             nums[nums.length - 1 - i] = temp;
-        }
+        
         }
         int i = piv+1;
         int j = nums.length-1;
