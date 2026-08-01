120eccce10840293773489733303929f601d6e19
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-01 18:48:21 +0530

    Add solution: 0128. Longest Consecutive Sequence (Medium)

--- 0128-longest-consecutive-sequence/Solution.java
+++ 0128-longest-consecutive-sequence/Solution.java
@@ -1,7 +1,8 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         int n=nums.length;
-        int longest 
+        int longest = 1;
+        int
         for(int i=0; i<n; i++){
             if(nums)
         }
