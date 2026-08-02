3780778c7cac95e765a438b50f7ee569d8daf55c
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-02 22:44:43 +0530

    Add solution: 0073. Set Matrix Zeroes (Medium)

--- 0073-set-matrix-zeroes/Solution.java
+++ 0073-set-matrix-zeroes/Solution.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    matrix[0][]
+                    matrix[0][j]=
                 }
             }
         }
