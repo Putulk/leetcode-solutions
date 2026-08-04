4fad9303c42ac2835312a192020b7c85cf94b88b
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-04 14:07:35 +0530

    Add solution: 0048. Rotate Image (Medium)

--- 0048-rotate-image/Solution.java
+++ 0048-rotate-image/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        for(int i=0; i<n-2; i++){
+        for(int i=0; i<n-; i++){
             for(int j=1; j<n-1; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
