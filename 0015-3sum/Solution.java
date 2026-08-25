dbae21ce87a1874b2a3ba95f6ead541afe529955
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 15:56:36 +0530

    Add solution: 0015. 3Sum (Medium)

--- 0015-3sum/Solution.java
+++ 0015-3sum/Solution.java
@@ -8,7 +8,7 @@ class Solution {
             for(int j=i+1; j<n; j++){
                 int thirdElement = -(nums[i]+nums[j]);
                 List<Integer> innerList = new ArrayList<>();
-                if(!set.contains(thirdElement)){
+                if(!set.contains(thirdElement) && i!=j && j!=k && i!=thirdElement){
                     innerList.add(nums[i]);
                     innerList.add(nums[j]);
                     innerList.add(thirdElement);
