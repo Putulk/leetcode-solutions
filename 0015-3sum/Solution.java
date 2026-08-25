f4e4dd96e1801f4bd3e01a9254c4ebf6c724d261
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 15:35:54 +0530

    Add solution: 0015. 3Sum (Medium)

--- 0015-3sum/Solution.java
+++ 0015-3sum/Solution.java
@@ -2,6 +2,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         int n= nums.length;
+        
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 int nums[] = (nums[i]+nums[j])
