56dbe00f6e55671bb4d59aeab6309e0208dbcfd9
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 22:21:07 +0530

    Add solution: 0018. 4Sum (Medium)

--- 0018-4sum/Solution.java
+++ 0018-4sum/Solution.java
@@ -9,7 +9,7 @@ class Solution {
                 int left = j+1;
                 int right = n-1;
                 while(left < right){
-                int sum = nums[i]+nums[j]+nums[le]
+                int sum = nums[i]+nums[j]+nums[left]+
                 if(sum < 0){
                     j++;
                 }else if(sum > 0){
