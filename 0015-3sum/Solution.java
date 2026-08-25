dbc2d4f4555baa96b9372c2e7b4521ef3170eb05
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 16:55:44 +0530

    Add solution: 0015. 3Sum (Medium)

--- 0015-3sum/Solution.java
+++ 0015-3sum/Solution.java
@@ -7,9 +7,9 @@ class Solution {
             int j = i+1;
             int k = n-1;
             
-            // if(nums[i] > 0 && nums[i] == nums[i-1]){
-            //     continue;
-            // }
+            if(nums[i] > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
             while(j<k){
                 if(nums[i]+nums[j]+nums[k] < 0){
                     j++;
