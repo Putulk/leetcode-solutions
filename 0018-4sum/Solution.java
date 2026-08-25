0abc5117fee745de8cd0e3d5f643c46ef5b1627b
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 22:18:08 +0530

    Add solution: 0018. 4Sum (Medium)

--- 0018-4sum/Solution.java
+++ 0018-4sum/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         for(int i=0; i<n-1; i++){
             if (i > 0 && arr[i] == arr[i - 1]) continue;
-            for
+            for(int j=)
             int j = i+1;
             int k = n-1;
             int fourth = target -(nums[i]+nums[j]+nums[k]);
