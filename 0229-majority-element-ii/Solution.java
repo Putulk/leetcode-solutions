4c38ce93d969b962d30148ab7611a9df85145985
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 15:05:55 +0530

    Add solution: 0229. Majority Element II (Medium)

--- 0229-majority-element-ii/Solution.java
+++ 0229-majority-element-ii/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
         int maj1=0, maj2=0;
-        int 
+        int c
         for(int i=0; i<n; i++){
 
         }
