6eb560d93b7315683e308c3920784bb80e9802a3
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 17:10:43 +0530

    Add solution: 0018. 4Sum (Medium)

--- 0018-4sum/Solution.java
+++ 0018-4sum/Solution.java
@@ -17,7 +17,7 @@ class Solution {
                     inner.add(nums[i]);
                     inner.add(nums[j]);
                     inner.add(nums[k]);
-                    inner.add(j)
+                    inner.add(f)
                 }
             }
 
