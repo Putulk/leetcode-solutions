2958007276b539a9b04f8a59c2a4351c398a33ec
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 17:04:57 +0530

    Add solution: 0018. 4Sum (Medium)

--- 0018-4sum/Solution.java
+++ 0018-4sum/Solution.java
@@ -6,7 +6,7 @@ class Solution {
             int j = i+1;
             int k = n-1;
             while(j < k){
-                M
+            
                 int comp = target - (nums[i]+nums[j]+nums[k]);
 
             }
