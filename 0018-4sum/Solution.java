c73a30e0345f78457893fa788bf3e2088a58a0d3
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 22:20:21 +0530

    Add solution: 0018. 4Sum (Medium)

--- 0018-4sum/Solution.java
+++ 0018-4sum/Solution.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0; i<n-1; i++){
             if (i > 0 && arr[i] == arr[i - 1]) continue;
             for(int j=i+1; j<n; j++){
+                
                 if (j > i+1 && arr[j] == arr[j - 1]) continue;
                 int left = j+1;
                 int right = n-1;
