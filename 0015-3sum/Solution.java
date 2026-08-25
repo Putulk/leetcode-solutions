be38a81ba154e501a31e04a2ffcf3851b7dbbae7
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 15:13:18 +0530

    Add solution: 0015. 3Sum (Medium)

--- 0015-3sum/Solution.java
+++ 0015-3sum/Solution.java
@@ -3,7 +3,9 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int n= nums.length;
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j+)
+            for(int j=; j<n; j++){
+
+            }
         }
     }
 }
