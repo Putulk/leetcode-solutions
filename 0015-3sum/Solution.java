079c5b36ecff8501edcff2c75fcba61a1ee54a68
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 16:41:19 +0530

    Add solution: 0015. 3Sum (Medium)

--- 0015-3sum/Solution.java
+++ 0015-3sum/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n= nums.length;
         Set<List<Integer>> ans = new HashSet<>();
-        int 
+        in
         return new ArrayList<>(ans);
     }
 }
