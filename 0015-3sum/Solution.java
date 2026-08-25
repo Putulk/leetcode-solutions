28e9f4105cdb624635eb94876431173e9df4c867
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 15:51:50 +0530

    Add solution: 0015. 3Sum (Medium)

--- 0015-3sum/Solution.java
+++ 0015-3sum/Solution.java
@@ -13,7 +13,7 @@ class Solution {
                     innerList.add(nums[j]);
                     innerList.add(thirdElement);
                     ans.add(innerList);
-                    
+                    C
                     return ans;
                 }else{
                     set.add(nums[j]);
