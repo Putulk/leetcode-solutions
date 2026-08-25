deed1e14e09a5791db9803639516b91d943c675d
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 16:46:40 +0530

    Add solution: 0015. 3Sum (Medium)

--- 0015-3sum/Solution.java
+++ 0015-3sum/Solution.java
@@ -20,7 +20,7 @@ class Solution {
                     inner.add(nums[k]);
                     and.add(inner);
                     j++;
-                    
+                    k-
                 }
             }
         }
