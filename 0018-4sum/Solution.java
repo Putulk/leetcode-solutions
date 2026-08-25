0263365e109ecfa148a7f3c4dd60dcdd32cfe93b
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 22:14:03 +0530

    Add solution: 0018. 4Sum (Medium)

--- 0018-4sum/Solution.java
+++ 0018-4sum/Solution.java
@@ -20,7 +20,7 @@ class Solution {
                     inner.add(fourth);
                     j++;
                     k--;
-                    while(j<k && nums[i) j++;
+                    while(j<k && ) j++;
                     while(j<k)
                 }
             }
