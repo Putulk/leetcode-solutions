11dc87c2f61ac2c7a4cbef198e99c3ea5d93d1d8
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-25 22:22:58 +0530

    Add solution: 0018. 4Sum (Medium)

--- 0018-4sum/Solution.java
+++ 0018-4sum/Solution.java
@@ -24,7 +24,7 @@ class Solution {
                     j++;
                     k--;
                     while(j<k && nums[left] == nums[left-1]) left++;
-                    while(j<k && nums[] == nums[right-1]) right--;
+                    while(j<k && nums[right] == nums[right-1]) right--;
                 }
             }
 
