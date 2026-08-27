8f0886b59df8e76f2593a982c84f2ccdf526461e
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-27 14:31:46 +0530

    Add solution: 560. Subarray Sum Equals K (Medium)

--- leetcode/0560-subarray-sum-equals-k/Solution.java
+++ leetcode/0560-subarray-sum-equals-k/Solution.java
@@ -6,6 +6,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<n; i++){
             sum += nums[i];
+            
             int freq = sum-k;
             if(map.containsKey(freq)){
                 cnt += map.get(freq);
