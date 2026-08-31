5580534caca0de3f008b893bbc8959dd46d97d4a
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-31 14:28:32 +0530

    Add solution: 88. Merge Sorted Array (Easy)

--- leetcode/0088-merge-sorted-array/Solution.java
+++ leetcode/0088-merge-sorted-array/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=m; i<n; i++){
+        for(int i=; i<n; i++){
             nums2[i] = nums1[i];
         }
     }
