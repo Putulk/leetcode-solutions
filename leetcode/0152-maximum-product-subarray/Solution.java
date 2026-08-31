4c5f27c0cef865554bd1fa2f74174b0067b7eb00
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-31 15:26:00 +0530

    Add solution: 152. Maximum Product Subarray (Medium)

--- leetcode/0152-maximum-product-subarray/Solution.java
+++ leetcode/0152-maximum-product-subarray/Solution.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0; i<n; i++){
             int prod *= nums[i];
             if(prod > max){
-                
+                max =
             }
         }
     }
