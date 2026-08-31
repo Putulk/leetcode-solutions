e9da2ce048ace9e5de42604aeb75b8620fee4b27
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-31 12:52:46 +0530

    Add solution: 56. Merge Intervals (Medium)

--- leetcode/0056-merge-intervals/Solution.java
+++ leetcode/0056-merge-intervals/Solution.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0; i<n; i++){
             int[] curr = intervals[0];
             if(curr[1] > intervals[i][0]){
-                curr[1] = Math.max(curr[1], interval[i][1]);
+                curr[1] = Math.max(curr[1], intervals[i][1]);
             }else{
                 list.add(curr);
                 curr = intervals[i];
