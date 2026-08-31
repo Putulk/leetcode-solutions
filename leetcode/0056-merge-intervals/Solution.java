e63e7001427fcee4dcb58dba4d1001e1b6f40d06
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-08-31 11:11:38 +0530

    Add solution: 56. Merge Intervals (Medium)

--- leetcode/0056-merge-intervals/Solution.java
+++ leetcode/0056-merge-intervals/Solution.java
@@ -14,7 +14,7 @@ class Solution {
                 j++;
             }
             list.add(Arrays.asList(start, end));
-            i=j;
+            i=j-1;
         }
         return list.toArray(new int[list.size()][]);
     }
