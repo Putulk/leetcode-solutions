a5293f2a1ee3aa6a27b1482798034e5bdc9a2ad0
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-09 17:18:58 +0530

    Add solution: 493. Reverse Pairs (Hard)

--- leetcode/0493-reverse-pairs/Solution.java
+++ leetcode/0493-reverse-pairs/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int reversePairs(int[] nums) {
         int n = nums.length;
         for(int i=0; i<n; i++){
-            for(int j=i+1;)
+            for(int j=i+1; j<n)
         }
     }
 }
