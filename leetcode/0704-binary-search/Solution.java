4e08f0d11c06b21937dd79b1e8a34832384ed6bf
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-12 15:11:38 +0530

    Add solution: 704. Binary Search (Easy)

--- leetcode/0704-binary-search/Solution.java
+++ leetcode/0704-binary-search/Solution.java
@@ -8,12 +8,12 @@ class Solution {
             int mid = left+(right-left)/2;
 
             if(nums[mid] == target){
-                a mid;
+                ans mid;
                 right = mid-1;
             }else {
                 left = mid+1;
             }
         }
-        return -1;
+        return an;
     }
 }
