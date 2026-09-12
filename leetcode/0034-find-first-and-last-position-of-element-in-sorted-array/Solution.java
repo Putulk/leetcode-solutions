5daa3666a5698d4754c3f09ff336fd11c2240689
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-12 17:24:25 +0530

    Add solution: 34. Find First and Last Position of Element in Sorted Array (Medium)

--- leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/Solution.java
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int left = 0;
         int right = n-1;
-        int[] 
+        int[] ans = 
 
         while(left <= right){
             int mid = left +(right-left)/2;
