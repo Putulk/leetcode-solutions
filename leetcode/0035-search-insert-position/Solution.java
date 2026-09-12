c81c3e10fb3a91dde732c720b370a8e28ecfec44
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-12 15:32:48 +0530

    Add solution: 35. Search Insert Position (Easy)

--- leetcode/0035-search-insert-position/Solution.java
+++ leetcode/0035-search-insert-position/Solution.java
@@ -9,7 +9,9 @@ class Solution {
 
             if(nums[mid] == target){
                 ans = target;
-            }else if(nums[mid] < target){}
+            }else if(nums[mid] < target){
+                
+            }
         }
     }
 }
