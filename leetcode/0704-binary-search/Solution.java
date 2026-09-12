f958222dcc94567f3f6251e5629ff712284d6199
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-12 14:58:27 +0530

    Add solution: 704. Binary Search (Easy)

--- leetcode/0704-binary-search/Solution.java
+++ leetcode/0704-binary-search/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int left = 0;
         int right = n-1;
-        while(l)
+        while(left <= ri)
             int mid = left+(right-left)/2;
 
             if(nums[mid] == target){
