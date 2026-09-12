1c70cb130a4f29b2416aec35cde1e7959605d305
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-12 14:54:08 +0530

    Add solution: 704. Binary Search (Easy)

--- leetcode/0704-binary-search/Solution.java
+++ leetcode/0704-binary-search/Solution.java
@@ -3,6 +3,8 @@ class Solution {
         int n=nums.length;
         int left = 0;
         int right = n-1;
-        fot(int i=0; i<n; i+)
+        fot(int i=0; i<n; i++){
+            i
+        }
     }
 }
