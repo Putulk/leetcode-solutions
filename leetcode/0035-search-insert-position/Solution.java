df058274146024150228d302ca0aa31e8837e3b8
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-12 15:40:54 +0530

    Add solution: 35. Search Insert Position (Easy)

--- leetcode/0035-search-insert-position/Solution.java
+++ leetcode/0035-search-insert-position/Solution.java
@@ -13,7 +13,7 @@ class Solution {
             }else if(nums[mid] < target){
                 left = mid+1;
             }else{
-                right = mi
+                right = mid-1;
             }
         }
     }
