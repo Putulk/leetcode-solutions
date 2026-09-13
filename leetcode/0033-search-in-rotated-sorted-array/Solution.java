db6149b67e015a1d93f5ea53a09845325964751b
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-13 23:02:22 +0530

    Add solution: 33. Search in Rotated Sorted Array (Medium)

--- leetcode/0033-search-in-rotated-sorted-array/Solution.java
+++ leetcode/0033-search-in-rotated-sorted-array/Solution.java
@@ -7,8 +7,7 @@ class Solution {
         while(left <= right){
             int mid = left+(right-left)/2;
             if(nums[mid] == target){
-\                    ans = mid;
-                }else if(nums[mid] > target){
+                    ans = mid;                }else if(nums[mid] > target){
                     left = mid+1;
                 }else{
                     right = mid-1;
