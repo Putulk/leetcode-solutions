8027752a490f9f34944fa86eea6017db0ba8a308
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-13 23:08:40 +0530

    Add solution: 33. Search in Rotated Sorted Array (Medium)

--- leetcode/0033-search-in-rotated-sorted-array/Solution.java
+++ leetcode/0033-search-in-rotated-sorted-array/Solution.java
@@ -16,7 +16,7 @@ class Solution {
                     left = mid+1;
                 }
             }else{
-                if(nums[right] < targe)
+                if(nums[right] < target &)
             }
         }
         return ans;
