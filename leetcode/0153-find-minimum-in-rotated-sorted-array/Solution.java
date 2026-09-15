927570616b432cf8136f89edd0d9cd13688ad987
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-15 13:28:51 +0530

    Add solution: 153. Find Minimum in Rotated Sorted Array (Medium)

--- leetcode/0153-find-minimum-in-rotated-sorted-array/Solution.java
+++ leetcode/0153-find-minimum-in-rotated-sorted-array/Solution.java
@@ -8,7 +8,7 @@ class Solution {
 
             if(nums[m] == min){
                 min = nums[min];
-            }else if(nums[m] > min && nu){
+            }else if(nums[m] > min && nums[m] ){
             }
         }
     }
