560b65cd8488b713ac033560fbea10eea39fed9f
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-16 12:22:47 +0530

    Add solution: 540. Single Element in a Sorted Array (Medium)

--- leetcode/0540-single-element-in-a-sorted-array/Solution.java
+++ leetcode/0540-single-element-in-a-sorted-array/Solution.java
@@ -11,7 +11,7 @@ class Solution {
             if(nums[mid] != nums[left] && nums[mid] != nums[right]){
                 ans = nums[mid];
                 left++;
-                rig
+                right--;
             }
         }
         return ans;
