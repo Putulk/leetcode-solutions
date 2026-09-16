367019587811ab3ae9afeb2ed43fa96a2173877a
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-16 12:26:07 +0530

    Add solution: 540. Single Element in a Sorted Array (Medium)

--- leetcode/0540-single-element-in-a-sorted-array/Solution.java
+++ leetcode/0540-single-element-in-a-sorted-array/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
         int left = 1;
-        int right = n-1;
+        int right = n-;
         int ans = 0;
 
         while(left <= right){
