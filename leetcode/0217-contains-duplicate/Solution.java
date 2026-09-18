75e8b085ee7133ed7190b1963669995f540bea7c
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-18 17:07:02 +0530

    Add solution: 217. Contains Duplicate (Easy)

--- leetcode/0217-contains-duplicate/Solution.java
+++ leetcode/0217-contains-duplicate/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         int n = nums.length;
-        Set<Intger> set = new HashS
+        Set<Intger> set = new HashSet<>();
         for(int i=0; i<n; i++){
 
         }
