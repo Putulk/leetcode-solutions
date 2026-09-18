302b045b564d8210025a0c2d1206488aa30b74bc
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-18 16:30:01 +0530

    Add solution: 1. Two Sum (Easy)

--- leetcode/0001-two-sum/Solution.java
+++ leetcode/0001-two-sum/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        Map<Integer,
+        Map<Integer, Intege
         for(int i=0; i<n; i++){
             int comp = target-nums[i];
         }
