9f72612d65d7e588068c9983f5de6a4b16d99e50
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-18 16:44:39 +0530

    Add solution: 121. Best Time to Buy and Sell Stock (Easy)

--- leetcode/0121-best-time-to-buy-and-sell-stock/Solution.java
+++ leetcode/0121-best-time-to-buy-and-sell-stock/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int profit
+        int profit = 0;
 
         for(int i=0; i<n; i++){
             int sellPrice = 
