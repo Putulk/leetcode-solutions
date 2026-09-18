4bf8ab9ddf0e4333772f4919caeb6c405c7f9941
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-18 17:08:17 +0530

    Add solution: 217. Contains Duplicate (Easy)

--- leetcode/0217-contains-duplicate/Solution.java
+++ leetcode/0217-contains-duplicate/Solution.java
@@ -6,7 +6,7 @@ class Solution {
             if(set.contains(nums[i])){
                 return true;
             }
-            set.a(nums[i]);
+            set.add(nums[i]);
         }
         return false;
     }
