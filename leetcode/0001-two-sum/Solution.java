747171797f80b3446232e0d5489e816a8b013229
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-18 16:31:09 +0530

    Add solution: 1. Two Sum (Easy)

--- leetcode/0001-two-sum/Solution.java
+++ leetcode/0001-two-sum/Solution.java
@@ -4,7 +4,9 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<n; i++){
             int comp = target-nums[i];
-            if(map.containsKey(comp)){}
+            if(map.containsKey(comp)){
+                r
+            }
         }
     }
 }
