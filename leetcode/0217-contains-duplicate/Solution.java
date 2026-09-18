9b05cd1759169420019bcf7727097d1c251f0658
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-18 17:18:30 +0530

    Add solution: 217. Contains Duplicate (Easy)

--- leetcode/0217-contains-duplicate/Solution.java
+++ leetcode/0217-contains-duplicate/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         Set<Integer> set = new HashSet<>();
         for(int i=0; i<n; i++){
-            if(set.contains(nums[i])){
+            if(!set.contains(nums[i])){
                 return true;
             }
             set.add(nums[i]);
