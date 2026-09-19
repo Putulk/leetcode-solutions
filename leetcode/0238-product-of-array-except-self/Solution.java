b65befed8329dc1dc0d4d052631131c714d85b57
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-19 15:24:04 +0530

    Add solution: 238. Product of Array Except Self (Medium)

--- leetcode/0238-product-of-array-except-self/Solution.java
+++ leetcode/0238-product-of-array-except-self/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int pr
+        int prod = 1;
         int[] temp = new int[nums.length];
         for(int i=0; i<nums.length; i++){
-            
+            prod = nums
         }
     }
 }
