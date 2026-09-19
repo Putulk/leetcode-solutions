4771d22cdc353ce911f3030475b5c5b8c154e5aa
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-19 18:20:28 +0530

    Add solution: 11. Container With Most Water (Medium)

--- leetcode/0011-container-with-most-water/Solution.java
+++ leetcode/0011-container-with-most-water/Solution.java
@@ -2,9 +2,10 @@ class Solution {
     public int maxArea(int[] height) {
         int left =0;
         int right = n-1;
+        
 
         for(int i =0; i<n; i++){
-            in
+            
         }
     }
 }
