4555a0d2b20ef6c12142d17459251fbe53926d81
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-19 15:12:05 +0530

    Add solution: 53. Maximum Subarray (Medium)

--- leetcode/0053-maximum-subarray/Solution.java
+++ leetcode/0053-maximum-subarray/Solution.java
@@ -5,10 +5,7 @@ class Solution {
 
         for(int i=0; i<n; i++){
             int sum = 0;
-            for(int j=i; j<n; j++){
-                sum += nums[j];
-                maxSum = Math.max(maxSum, sum);
-            }
+            
 
         }
         return maxSum;
