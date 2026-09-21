5b860544f0a34e2928876138a239939a3007c5ae
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-21 16:17:24 +0530

    Add solution: 73. Set Matrix Zeroes (Medium)

--- leetcode/0073-set-matrix-zeroes/Solution.java
+++ leetcode/0073-set-matrix-zeroes/Solution.java
@@ -4,7 +4,9 @@ class Solution {
         int n = matrix[0].length;
 
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){}
+            for(int j=0; j<n; j++){
+                
+            }
         }
     }
 }
