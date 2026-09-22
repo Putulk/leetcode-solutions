83c70d773e883ee836aaaeef4582f67bd972bb37
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-22 12:29:16 +0530

    Add solution: 54. Spiral Matrix (Medium)

--- leetcode/0054-spiral-matrix/Solution.java
+++ leetcode/0054-spiral-matrix/Solution.java
@@ -25,7 +25,7 @@ class Solution {
                 bottom--;
             }
             
-            if(le)
+            if(left <= )
             for(int i=bottom; i>=top; i--){
                 res.add(matrix[i][left]);
             }
