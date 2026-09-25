aa64821c67de5a73c0f6bba98d6ceba9b8ae7171
Putul Kumari <89597576+Putulk@users.noreply.github.com>
2026-09-25 11:31:53 +0530

    Add solution: 2656. Maximum Sum With Exactly K Elements  (Easy)

--- leetcode/2656-maximum-sum-with-exactly-k-elements/Solution.java
+++ leetcode/2656-maximum-sum-with-exactly-k-elements/Solution.java
@@ -9,7 +9,7 @@ class Solution {
             if(nums[l] <= nums[r]){
                 m = nums[r];
             }
-            score = score+nums[n];
+            score = score+nums[n-];
             nums[n] = m+1;
         }
         return score;
